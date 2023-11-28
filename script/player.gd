13a46aff6e0059759600b9909b9721f6c431a65b
Maykon Facincani <facincani.maykon@gmail.com>
2023-11-28 09:17:50 -0300

    First Episode
    Player Animations
     - 8 Directions (S/SE/E/NE/N/NW/W/SW)
     - Walk
     - Idle

---
+++ script/player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+@onready var animSprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var speed = 100
+var player_state: String = "idle"
+var player_last_dir: String = "s"
+
+func _physics_process(delta):
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	if direction.x == 0 and direction.y == 0:
+		player_state = "idle"
+	elif direction.x != 0 || direction.y != 0:
+		player_state = "walk"
+	
+	velocity = direction * speed
+	move_and_slide()
+	
+	checkDirection(direction)
+	play_anim(direction)
+	
+func play_anim(playerDirection: Vector2):
+	animSprite.play(player_last_dir + "-" + player_state)
+
+
+func checkDirection(playerDirection: Vector2):
+	if playerDirection.y == -1:
+		player_last_dir = "n"
+	if playerDirection.x == 1:
+		player_last_dir = "e"
+	if playerDirection.y == 1:
+		player_last_dir = "s"
+	if playerDirection.x == -1:
+		player_last_dir = "w"
+		
+	if playerDirection.x > 0.5 and playerDirection.y < -0.5:
+		player_last_dir = "ne"
+	if playerDirection.x > 0.5 and playerDirection.y > 0.5:
+		player_last_dir = "se"
+	if playerDirection.x < -0.5 and playerDirection.y < -0.5:
+		player_last_dir = "nw"
+	if playerDirection.x < -0.5 and playerDirection.y > 0.5:
+		player_last_dir = "sw"
